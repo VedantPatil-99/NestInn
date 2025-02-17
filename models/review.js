c71c333e1b0e8ca6661f35581d1ca28be20cb72b
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-02-17 22:59:51 +0530

    Reviews comment maxlength extend

---
+++ models/review.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+	comment: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 3,
+		maxlength: 2000,
+	},
+	rating: {
+		type: Number,
+		min: 1,
+		max: 5,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	listing: {
+		type: Schema.Types.ObjectId,
+		ref: "Listing",
+		required: true,
+	},
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
