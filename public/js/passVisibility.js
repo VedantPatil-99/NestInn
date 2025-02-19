8defd19dff68a3808ef593cd3db8104dac1170f0
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-02-19 15:58:14 +0530

    Add login and signup forms with password visibility toggle

---
+++ public/js/passVisibility.js
@@ -0,0 +1,12 @@
+document
+	.getElementById("togglePassword")
+	.addEventListener("click", function () {
+		const passwordInput = document.getElementById("password");
+		if (passwordInput.type === "password") {
+			passwordInput.type = "text";
+			this.classList.replace("fa-eye", "fa-eye-slash");
+		} else {
+			passwordInput.type = "password";
+			this.classList.replace("fa-eye-slash", "fa-eye");
+		}
+	});
